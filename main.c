e89b1367efa996f6ef5e29f508707a738e5abff5
mohamedaitelkadi <mohamedaitelkadi001@gmail.com>
2022-09-13 15:11:48 +0100

    3 types of SORT

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+#include<windows.h>
+//declare de variables:
+int i,j,size,s;
+int t[100];
+//------------------------------------------------------------------------------------------------
+//TRI A BULLE:
+void bubble_sort(){
+for(i=0;i<size;i++)
+        for(j=0;j<size-1;j++)
+            if(t[j]>t[j+1]){
+                s = t[j];
+                t[j] = t[j+1];
+                t[j+1] = s;
+            }
+}
+//-------------------------------------------------------------------------------------------------
+//TRI PAR INSERTION:
+void insert_sort(){
+    for (i=1;i<=size-1;i++) {
+    j=i;
+    while (j>0 && t[j-1]>t[j]){
+      s=t[j];
+      t[j]=t[j-1];
+      t[j-1]=s;
+      j--;
+    }
+   }
+}
+//------------------------------------------------------------------------------------------------------------
+//TRI PAR SELECTION :
+void select_sort(){
+    for(i=0;i<size;i++)
+        for(j=i+1;j<size;j++)
+            if(t[i]>t[j]){
+                s = t[i];
+                t[i]= t[j];
+                t[j]= s;
+            }
+
+}
+//----------------------------------------------------------------------------------------------------
+int main()
+{
+    //
+    printf("entrer la taille de tableau:\n");
+    scanf("%d",&size);
+    printf("entrer les %d nombres en ce tableau:\n",size);
+    for(i=0;i<size;i++){
+        printf("entrer nombre n%d : ",i+1);
+        scanf("%d",&t[i]); // pour remplir le tableau
+        printf("\n");
+    }
+    for(i=0;i<size;i++){ // affichage de tableau:
+        printf("nombre n%d:%d\n",i+1,t[i]);
+    }
+    bubble_sort();
+    //insert_sort();
+    //select_sort();
+    printf("le tableau apres le tri : \n");
+    for(i=0;i<size;i++){ // affichage de tableau
+        printf("nombre n%d: %d\n",i+1,t[i]);
+    }
+
+    return 0;
+}
